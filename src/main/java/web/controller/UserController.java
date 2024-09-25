c3a2fdc55fb7a1a687e4f4b10b37d6c12ab5b036
YADJUN <m.kibiev47@gmail.com>
2024-09-25 04:31:04 +0300

    8:08

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -2,10 +2,7 @@ package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import web.service.UserService;
 import web.model.User;
 
@@ -36,20 +33,20 @@ public class UserController {
         return "redirect:/index";
     }
 
-    @PostMapping("/update/{id}")
-    public String updateUser(@PathVariable int id, @ModelAttribute User user) {
+    @PostMapping("/update/")
+    public String updateUser(@RequestParam int id, @ModelAttribute User user) {
         userService.updateUser(id, user);
         return "redirect:/index";
     }
 
-    @PostMapping("/delete/{id}")
-    public String deleteUser(@PathVariable int id) {
+    @PostMapping("/delete/")
+    public String deleteUser(@RequestParam int id) {
         userService.deleteUser(id);
         return "redirect:/index";
     }
 
-    @GetMapping("/update/{id}")
-    public String showUpdateForm(@PathVariable("id") int id, Model model) {
+    @GetMapping("/update/")
+    public String showUpdateForm(@RequestParam("id") int id, Model model) {
         User user = userService.getUserById(id);
         model.addAttribute("user", user);
         return "update";
